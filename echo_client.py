205c1fe298be8f71f07fdf89c59ece9ef50e9620
Hirt Felix <stbu0422@schneiderpen.com>
2023-03-07 17:04:49 +0100

    Initial commit

---
+++ echo_client.py
@@ -0,0 +1,74 @@
+import socket
+import sys
+
+client_socket = socket.socket()
+LOCALHOST = '192.168.178.104'
+PORT = 4242
+
+
+try:
+    host = sys.argv[1]  # server IP given on command line
+except IndexError:
+    host = LOCALHOST
+    print(f"No server given on command line. Trying {host}")
+try:
+    print(f"Waiting for connection to {host}")
+    client_socket.connect((host, PORT))
+except socket.error as e:
+    print(str(e))
+    if host == LOCALHOST:
+        print("Running locally on Debian based system? "
+              "Then you may try 127.0.1.1 for connecting to server")
+    sys.exit(1)
+
+# Ask for connection
+response = client_socket.recv(2048)
+print(f"{response.decode()}")
+
+# Send username
+response = client_socket.recv(2048)
+print(f"{response.decode()}")
+username = input("Username: ")
+client_socket.send(str.encode(username))
+
+# Wait for registration confirmation
+response = client_socket.recv(2048)
+print(f"{response.decode()}")
+
+# Do the talk
+while True:
+    print("====== Minimal Chat System ======")
+    print("1: Send message")
+    print("2: Check incoming messages")
+    print("3: List online clients")
+    print("4: Quit")
+    msg = input("Your Selection: ")
+
+    if not msg:  # empty message not permitted in TCP
+        continue
+    client_socket.send(str.encode(msg))
+    if "stop" in msg.lower():  # protocol: "stop" to close connection.
+        break
+
+    elif msg == "1":
+        recipient = input("Send message to: ")
+        message = input("Your message: ")
+        client_socket.send(str.encode(f"{recipient} {message}"))
+
+    elif msg == "2":
+        print("Checking incoming Messages...")
+        client_socket.send(str.encode(msg))
+        num_messages = client_socket.recv(2048).decode()
+        print(f"You have {num_messages} new message(s).")
+        for i in range(int(num_messages)):
+            incoming_msg = client_socket.recv(2048).decode()
+            print(f"{incoming_msg} \n")
+    else:
+        print("Listening ...")
+        # Echo from Server
+        response = client_socket.recv(2048)
+        return_msg = response.decode()
+        print(return_msg)
+
+print("Closing connection")
+client_socket.close()
---
+++ echo_server.py
@@ -0,0 +1,109 @@
+import socket
+import sys
+import platform
+from threading import Thread
+
+PORT = 4242
+message_dict = {}
+
+def get_my_ip():
+    """Return the ipaddress of the local host
+
+    On Debian-based systems we may get a rather strange address: 127.0.1.1
+    See: https://www.debian.org/doc/manuals/debian-reference/ch05.en.html#_the_hostname_resolution
+
+    """
+    return socket.gethostbyname(socket.gethostname())
+
+
+def threaded_client(connection, address):
+    connection.send(str.encode("Please send username"))
+    data = connection.recv(2048)
+    username = data.decode()
+    message_dict[username] = []  # initialize empty list for user
+    message = data.decode()
+    print(f"{username} at {address[0]}:{address[1]}")
+
+    # Send registration confirmation
+    connection.send(str.encode(f"Registered as {username}"))
+
+    while True:
+        print(f"Listening for {address[0]}:{address[1]} ...")
+        data = connection.recv(2048)
+        message = data.decode()
+        if message == '1':
+            # Receive recipient and message from client
+            data = connection.recv(2048).decode()
+            recipient, message = data.split(" ", 1)
+            # Add message to recipient's message list
+            if recipient in message_dict:
+                message_dict[recipient].append(f"{username}: {message}")
+            print(message_dict)
+
+        elif message == '2':  # client requests incoming messages
+            incoming_messages = message_dict[username]
+            connection.send(str.encode(str(len(incoming_messages))))  # send number of messages
+
+            for msg in incoming_messages:
+                connection.send(str.encode(msg))
+            message_dict[username] = []  # clear the list of incoming messages
+
+        elif "stop" in message.lower():
+            break
+        else:  # assume it's a regular message
+            reply = f"Echo to {username}: {message}"
+            connection.send(str.encode(reply))
+            # add message to recipient's message list
+            if message.startswith("@"):  # direct message
+                recipient = message.split()[0][1:]
+                if recipient in message_dict:
+                    message_dict[recipient].append(f"{username}: {message}")
+            else:  # broadcast message
+                for recipient in message_dict:
+                    if recipient != username:
+                        message_dict[recipient].append(f"{username}: {message}")
+
+    print(f"Closing connection to {address[0]}:{address[1]}")
+    del message_dict[username]
+    connection.close()
+
+
+if __name__ == "__main__":
+    # Socket setup
+    server_socket = socket.socket()
+    try:
+        server_socket.bind((get_my_ip(), PORT))
+    except socket.error as e:
+        print(e)
+        server_socket.close()
+        sys.exit(1)
+    if platform.system() == "Windows":
+        # Windows: No reaction to signals during socket.accept()
+        #          => Wake up periodically from accept.
+        server_socket.settimeout(1)
+    # Start socket
+    server_socket.listen()
+
+    print("===== Start Chat Server =====")
+    print(f"Starting Server on {get_my_ip()}:{PORT}")
+    print("Server running")
+    # Accept connections
+    conn_cnt = 0
+    try:
+        while True:
+            try:
+                # print("Ready for a new connection ...\n")
+                conn, addr = server_socket.accept()
+                msg = f"New connection: {addr[0]}:{addr[1]}"
+                print(msg)
+                conn.send(msg.encode())
+                Thread(target=threaded_client, args=(conn, addr), daemon=True).start()
+                conn_cnt += 1  # TODO: De-register connections
+                print(f"{conn_cnt} connections")
+            except socket.timeout:  # Windows (Python >= 3.10: TimeoutError)
+                continue
+    except KeyboardInterrupt:
+        print("\nStopping server due to user request")
+    finally:
+        print("Closing server socket")
+        server_socket.close()
